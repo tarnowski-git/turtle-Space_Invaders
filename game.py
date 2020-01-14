52ea921779f23d571584432dba4f960670b0d536
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-15 00:28:39 +0100

    Add a player

--- game.py
+++ game.py
@@ -4,7 +4,28 @@ import os
 # Set up the screen
 wn = turtle.Screen()
 wn.bgcolor("black")
+wn.setup(width=650, height=650)
 wn.title("Space Invaders")
 
+# Draw border
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300, -300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range(4):
+    border_pen.forward(600)
+    border_pen.left(90)
+
+# Create the player turtle
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+player.setheading(90)
 
 delay = input("Press enter to finish")
