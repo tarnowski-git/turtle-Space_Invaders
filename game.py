081433b80bc0b0b3e75d036a8d475f415a219882
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-19 23:24:31 +0100

    Add scoring

--- game.py
+++ game.py
@@ -21,6 +21,19 @@ for side in range(4):
     border_pen.forward(600)
     border_pen.left(90)
 
+# Set the score to 0
+score = 0
+
+# Draw the score
+score_pen = turtle.Turtle()
+score_pen.speed(0)
+score_pen.color("white")
+score_pen.penup()
+score_pen.setposition(-290, 280)
+scoreString = "Score: %s" % score
+score_pen.write(scoreString, False, align="left", font=("Arial", 14, "normal"))
+score_pen.hideturtle()
+
 # Create the player turtle
 player = turtle.Turtle()
 player.color("blue")
@@ -158,6 +171,12 @@ while True:
             x = random.randint(-200, 200)
             y = random.randint(100, 250)
             enemy.setposition(x, y)
+            # Update the score
+            score += 10
+            scoreString = "Score: %s" % score
+            score_pen.clear()
+            score_pen.write(scoreString, False, align="left",
+                            font=("Arial", 14, "normal"))
 
         if isCollision(player, enemy) == True:
             player.hideturtle()
